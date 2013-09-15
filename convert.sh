424190eafb5e8d2e0827a5945c24d969f1d04bdb
Shichao An <moleculeandrew@gmail.com>
2013-09-15 18:12:59 -0400

    check for original video file type: mp4

--- convert.sh
+++ convert.sh
@@ -5,11 +5,11 @@ then
 	echo $usage
 	exit 1
 fi
-flvs=($(ls $1/*.flv))
+flvs=($(ls ${1%/}/*.flv))
 for flv in "${flvs[@]}"
 do
 	bn=$(basename -s .flv $flv)
 	mp4="$bn.mp4"
 	echo "Converting $flv..."
-	ffmpeg -i $flv -c:a copy -sameq $1/$mp4
+	ffmpeg -i $flv -c:a copy -sameq ${1%/}/$mp4
 done
--- fetch.sh
+++ fetch.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 usage="./$0 <input_file> <output_directory>"
+get_flash_videos="/usr/local/bin/get_flash_videos"
 if [ "$#" -lt "2" ]
 then
 	echo $usage
@@ -9,7 +10,14 @@ while read url; do
 	if [[ -n $url ]]; then
 		real_url=$(curl $url -s -L -I -o /dev/null -w '%{url_effective}')
 		echo $real_url
-		/usr/local/bin/get_flash_videos $real_url -f $2/$(date +"%H%M%S-%m%d%y").flv
+		filename=$($get_flash_videos -info $real_url | grep 'Filename:' \
+			| awk '{ print $2 }')
+		ext=${filename##*.}
+		if [ "$ext" == "mp4" ]; then
+			$get_flash_videos $real_url -f ${2%/}/$filename
+		else
+			$get_flash_videos $real_url -f ${2%/}/$(date +"%H%M%S-%m%d%y").flv
+		fi
 		sleep 0.5
 	fi
 done < $1
